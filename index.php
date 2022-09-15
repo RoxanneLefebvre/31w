faa3fcfc93ac91ceb3531ee7f0d833e587d7bbdc
RoxanneLefebvre <102246244+RoxanneLefebvre@users.noreply.github.com>
2022-09-15 12:02:24 -0400

    ajout de plusieur fichier base, variables et generic

--- index.php
+++ index.php
@@ -15,28 +15,23 @@
 
 ?>
 
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="./style.css">
-    
-    <title>Document</title>
-    
-</head>
-<body>
-    <main>
-        <?php 
-            if ( have_posts() ) : 
-                while ( have_posts() ) :
-                    the_post();
-                    the_title('<h1>', '</h1>');
-                    the_content(null, true);
-                endwhile;
-            endif;
-        ?>
-    </main>
-</body>
-</html>
+    <?php
+        get_header();
+    ?>
+
+        <main>
+            <?php 
+                if ( have_posts() ) : 
+                    while ( have_posts() ) :
+                        the_post();
+                        the_title('<h1>', '</h1>');
+                        the_content(null, true);
+                    endwhile;
+                endif;
+            ?>
+        </main>
+
+    <?php
+        get_footer();
+    ?>
+
